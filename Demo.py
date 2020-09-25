8d836fabb33ff41c37f2ac05354cfc9e03db32de
quanghuy2604 <47911781+quanghuy2604@users.noreply.github.com>
2020-09-25 17:51:46 +0700

    Update Demo.py

--- Demo.py
+++ Demo.py
@@ -1,27 +1,38 @@
 from tkinter import *
 from tkinter import messagebox
-
+from tkcalendar import Calendar,DateEntry
+from datetime import timedelta
+import datetime
 
 window = Tk()
+now_date = datetime.datetime.now()
 
 window.title("Welcome App")
 
-window.geometry('280x50')
-
-lbl = Label(window, text="Input your Name:  ")
-
+window.geometry('380x90')
+#Row1
+lbl = Label(window, text="   Input your Name:  ")
 lbl.grid(column=0, row=0)
 
 txt = Entry(window, width=20)
-
 txt.grid(column=1, row=0)
+#Row2
+lb_date = Label(window, text ="Input your birth: ")
+lb_date.grid(column=0, row=2)
 
+cal = DateEntry(font="Arial 14",year=1999,date_pattern='dd/mm/y')
+cal.grid(column=1,row=2)
 
 def clicked():
-    messagebox.showinfo('Message title', 'Welcome to Tkinter, '+ txt.get())
+    delta = abs(now_date.date() - cal.get_date())
+    messagebox.showinfo('Message title', 'Welcome to Tkinter, ' + txt.get())
+    if now_date.date() != cal.get_date():
+        messagebox.showinfo('Message title', ' You lived in this world ' + str(delta))
+    else:
+        messagebox.showinfo('Message title', 'Welcome to the World, You lived in this world ')
+
 
 btn = Button(window, text="Accept", command=clicked)
-
-btn.grid(column=1, row=1)
+btn.grid(column=1, row=3)
 
 window.mainloop()
